b537631b851454a753992ff155b944ac9bb305eb
Farmaan Elahi <farmaan@triveous.com>
2025-06-28 03:06:07 +0530

    Added theme layout

--- src/content/trading_view/App.tsx
+++ src/content/trading_view/App.tsx
@@ -2,11 +2,14 @@ import "@/global.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/lib/client";
 import { DashboardLayout } from "@/components/dashboard/layout";
+import { ThemeProvider } from "@/components/theme/theme-provider.tsx";
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <DashboardLayout />
+      <ThemeProvider>
+        <DashboardLayout />
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
